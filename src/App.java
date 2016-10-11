33e3b62069826b369163bb1dc0eee41b7032702c
Anders W. Olsen <awo@defalt.net>
2016-10-11 16:26:26 +0200

    Added test tests

---
+++ src/App.java
@@ -0,0 +1,12 @@
+/**
+ * Created by awo on 11/10/16.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println(add(5,4));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
