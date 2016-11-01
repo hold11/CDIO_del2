2cb7719d94bff156b71b9aaf800e5d773916fe36
trules <troelsjc@gmail.com>
2016-11-01 16:02:08 +0100

    added GameLogic.java

---
+++ src/GameLogic.java
@@ -0,0 +1,27 @@
+/**
+ * Created by tjc on 1/11/16.
+ */
+/*
+           ,                                             |
+          /#\         _         _     _    ___   ___     | Projekt: Black Darkness 3 - CDIO_del2
+         /###\       | |__   _ | | __| |  /_  | /_  |    | Version: v1.0.0
+        /#####\      | '_ \ / \| |/ _  |    | |   | |    |
+       /##,-,##\     | | | | O | | ( | |   _| |_ _| |_   | Anders Wiberg Olsen (s165241), Emil Johan Høj (s152282),
+      /##(   )##\    |_| |_|\_/|_|\__,_|  |_____|_____|  | Iman Chelhi (s165228), Troels Just Christoffersen (s120052)
+     /#.--   --.#\                                       | Sebastian Tibor Bakonyvári (s145918), Valentin Leon Christensen (s152735)
+    /`           ´\                                      |
+ */
+
+public class GameLogic {
+    private DiceCup cup = new DiceCup();
+
+    private int currentPlayer;
+
+    public void playTurn(Player){
+
+    }
+
+    public void nextPlayer(){
+
+    }
+}
