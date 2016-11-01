d0a002e043369ffbd7ca5418ae7fed0c86744ae0
Anders W. Olsen <awo@defalt.net>
2016-11-01 17:27:16 +0100

    Example of players bank account
    Example on how to deposit to a player’s bank account.

--- src/App.java
+++ src/App.java
@@ -4,5 +4,8 @@
 public class App {
     public static void main(String[] args) {
         Die d = new Die(12);
+        Player p = new Player();
+        
+        p.getPlayerAccount().deposit(3);
     }
 }
