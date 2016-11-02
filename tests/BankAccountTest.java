1168714019538747ebddeb7c19d042dc6ac9a275
Emilhoeg <emilhoeg@hotmail.com>
2016-11-02 16:21:31 +0100

    JUnit test
    Test af setBalance og getBalance.

--- tests/BankAccountTest.java
+++ tests/BankAccountTest.java
@@ -8,25 +8,39 @@ import static org.junit.Assert.*;
  * Created by emilh on 02-11-2016.
  */
 public class BankAccountTest {
+    private BankAccount playerAccount;
+    private Player p1, p2;
+    private final int START_BALANCE = 300;
 
     @Before
     public void setUp() throws Exception {
+        p1 = new Player();
+        p2 = new Player();
 
 
     }
 
     @After
     public void tearDown() throws Exception {
+        Player.reset();
+
 
     }
 
     @Test
     public void setBalance() throws Exception {
+        p1.getPlayerAccount().setBalance(100);
+        p2.getPlayerAccount().setBalance(250);
+
+        assertEquals(100, p1.getPlayerAccount().getBalance());
+        assertEquals(250, p2.getPlayerAccount().getBalance());
 
     }
 
     @Test
     public void getBalance() throws Exception {
+        assertEquals(300, p1.getPlayerAccount().getBalance());
+        assertEquals(300, p2.getPlayerAccount().getBalance());
 
     }
 
