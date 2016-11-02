ebe2f510c9cf716cc32691c7ff4107b34f790f62
Emilhoeg <emilhoeg@hotmail.com>
2016-11-02 15:23:17 +0100

    bankAccount test
    Test af bankAccount

---
+++ tests/BankAccountTest.java
@@ -0,0 +1,43 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by emilh on 02-11-2016.
+ */
+public class BankAccountTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void setBalance() throws Exception {
+
+    }
+
+    @Test
+    public void getBalance() throws Exception {
+
+    }
+
+    @Test
+    public void deposit() throws Exception {
+
+    }
+
+    @Test
+    public void withdraw() throws Exception {
+
+    }
+
+}
